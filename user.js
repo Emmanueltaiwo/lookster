90a7e771286ac27b7861ad9cd1301cbcba783a75
Emmanueltaiwo <77551348+Emmanueltaiwo@users.noreply.github.com>
2022-07-01 14:06:32 -0700

    INITIAL COMMIT

---
+++ user.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+$(document).ready(function() {
+    if (window.File && window.FileList && window.FileReader) {
+      $("#files").on("change", function(e) {
+        var files = e.target.files,
+          filesLength = files.length;
+        for (var i = 0; i < filesLength; i++) {
+          var f = files[i]
+          var fileReader = new FileReader();
+          fileReader.onload = (function(e) {
+            var file = e.target;
+            $("<span class=\"pip\">" +
+              "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
+              "<br/><span class=\"remove\">Remove image</span>" +
+              "</span>").insertAfter("#files");
+            $(".remove").click(function(){
+              $(this).parent(".pip").remove();
+            });
+            
+            // Old code here
+            /*$("<img></img>", {
+              class: "imageThumb",
+              src: e.target.result,
+              title: file.name + " | Click to remove"
+            }).insertAfter("#files").click(function(){$(this).remove();});*/
+            
+          });
+          fileReader.readAsDataURL(f);
+        }
+        console.log(files);
+      });
+    } else {
+      alert("Your browser doesn't support to File API")
+    }
+  });
+
+
+  let rateoneEl = document.getElementById("rate-one-el")
+let ratetwoEl = document.getElementById("rate-two-el")
+
+  console.log(rateEl)
+
+  let count = 0
+
+   function compare() {
+  rateoneEl.textContent = Math.floor(Math.random() * 100) + "%";
+  ratetwoEl.textContent = Math.floor(Math.random() * 100) + "%";
+
+   }
